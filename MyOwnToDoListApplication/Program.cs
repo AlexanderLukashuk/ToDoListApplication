d44618a9857efcc5ca4442b4315fa51a0d3f77c0
AlexanderLukashuk <upiter465@gmail.com>
2023-03-06 00:18:17 +0600

    Created builder and connectionString to connect to database

--- MyOwnToDoListApplication/Program.cs
+++ MyOwnToDoListApplication/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+
 Console.WriteLine("Hello, World!");
 
+var builder = new ConfigurationBuilder();
+
+builder.SetBasePath(Directory.GetCurrentDirectory());
+builder.AddJsonFile("appsettings.json");
+
+var config = builder.Build();
+
+string connectionString = config.GetConnectionString("DefaultConnection");

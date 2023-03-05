0e8b2949f3cb4f56e42b5ad93e6347a2a2a7acb2
AlexanderLukashuk <upiter465@gmail.com>
2023-03-05 18:28:01 +0600

    Added ClassLibrary project and created two classes: ToDo and SubToDo

---
+++ MyOwnToDoListApplicationLibrary/SubToDo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace MyOwnToDoListApplicationLibrary
+{
+	public class SubToDo
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public string Description { get; set; } = string.Empty;
+		public DateTime Deadline { get; set; }
+		public bool Status { get; set; }
+		public int ToDoId { get; set; }
+
+		public SubToDo()
+		{
+		}
+	}
+}
+
---
+++ MyOwnToDoListApplicationLibrary/ToDo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace MyOwnToDoListApplicationLibrary
+{
+	public class ToDo
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public double Progress { get; set; }
+
+		public ToDo()
+		{
+		}
+	}
+}
+

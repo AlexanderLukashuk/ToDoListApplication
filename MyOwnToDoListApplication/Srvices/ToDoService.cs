cfaa23f9c96669e666ba51572c14dc6059f74ab3
AlexanderLukashuk <upiter465@gmail.com>
2023-03-05 20:39:03 +0600

    Created method GetAllToDo

--- MyOwnToDoListApplication/Srvices/ToDoService.cs
+++ MyOwnToDoListApplication/Srvices/ToDoService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data.SqlClient;
+using MyOwnToDoListApplicationLibrary;
 
 namespace MyOwnToDoListApplication.Srvices
 {
@@ -95,6 +96,46 @@ namespace MyOwnToDoListApplication.Srvices
 					connection.Close();
 				}
 			}
+		}
+
+		public List<ToDo> GetAllToDo(string connectionString)
+		{
+			var todoList = new List<ToDo>();
+
+			using (var connection = new SqlConnection(connectionString))
+			{
+				try
+				{
+					connection.Open();
+
+					string query = "SELECT * FROM ToDo";
+
+					using (var command = new SqlCommand(query, connection))
+					{
+						using (var reader = command.ExecuteReader())
+						{
+							while (reader.Read())
+							{
+								var todoID = reader.GetInt32(reader.GetOrdinal("Id"));
+								var name = reader.GetString(reader.GetOrdinal("Name"));
+								var progress = reader.GetDouble(reader.GetOrdinal("Progress"));
+
+								todoList.Add(new ToDo { Id = todoID, Name = name, Progress = progress });
+							}
+						}
+					}
+				}
+				catch (SqlException ex)
+				{
+					Console.WriteLine("ERROR: " + ex.Message);
+				}
+				finally
+				{
+					connection.Close();
+				}
+			}
+
+			return todoList;
 		}
 	}
 }

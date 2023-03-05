cc177d2616fc8bee9d851f6cd7290d7a489256c4
AlexanderLukashuk <upiter465@gmail.com>
2023-03-05 18:58:23 +0600

    Created folder Services, ToDoService class, method CreateToDo and added nuget packages for SqlClient

---
+++ MyOwnToDoListApplication/Srvices/ToDoService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace MyOwnToDoListApplication.Srvices
+{
+	public class ToDoService
+	{
+		public ToDoService()
+		{
+		}
+
+		public void CreateToDo(string name, double progress, string connectionString)
+		{
+			using (var connecction = new SqlConnection(connectionString))
+			{
+				try
+				{
+					connecction.Open();
+
+					string query =
+                        "INSERT INTO ToDo (Name, Progress)" +
+                        "VALUES (@name, @progress);";
+
+					using (var command = new SqlCommand(query, connecction))
+					{
+						command.Parameters.AddWithValue("@name", name);
+                        command.Parameters.AddWithValue("@progress", progress);
+
+						command.ExecuteNonQuery();
+                    }
+                }
+				catch (SqlException ex)
+				{
+					Console.WriteLine("ERROR: Can't connect to service " + ex.Message);
+				}
+				finally
+				{
+					connecction.Close();
+				}
+            }
+		}
+	}
+}
+

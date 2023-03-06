d808e956db78a1e0768e90e7974c67b940afe7e2
AlexanderLukashuk <upiter465@gmail.com>
2023-03-06 14:53:31 +0600

    Started creating switch-case for user interface, created case 1: CreateToDo

--- MyOwnToDoListApplication/Program.cs
+++ MyOwnToDoListApplication/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.Extensions.Configuration;
+using MyOwnToDoListApplication.Srvices;
+using MyOwnToDoListApplicationLibrary;
 
 Console.WriteLine("Hello, World!");
 
@@ -11,3 +13,66 @@ builder.AddJsonFile("appsettings.json");
 var config = builder.Build();
 
 string connectionString = config.GetConnectionString("DefaultConnection");
+
+int menu = -3;
+string? input;
+ToDoService todoService = new ToDoService();
+SubToDoService subtodoService = new SubToDoService();
+List<ToDo> todos;
+List<SubToDo> subtodos;
+
+while (menu != 123)
+{
+    Console.Clear();
+    todos = todoService.GetAllToDo(connectionString);
+
+    Console.WriteLine("Choose option:");
+    Console.WriteLine("1) Create ToDo");
+    Console.WriteLine("2) Delete ToDO");
+    Console.WriteLine("3) Update ToDo(set name)");
+    Console.WriteLine("4) Get All ToDo");
+    Console.WriteLine("5) Get ToDo by name");
+    Console.WriteLine("6) Exit");
+
+    input = Console.ReadLine();
+
+    if (!int.TryParse(input, out menu) || string.IsNullOrEmpty(input))
+    {
+        Console.WriteLine("Input incorrect. Try Again");
+        continue;
+    }
+
+    Console.Clear();
+
+    switch (menu)
+    {
+        case 1:
+            {
+                Console.Write("Enter ToDo name: ");
+                string? name = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(name))
+                {
+                    Console.WriteLine("ToDo name can't be null or empty");
+                }
+                else
+                {
+                    ToDo? tempToDo = todos.Find(t => t.Name == name);
+
+                    if (tempToDo != null)
+                    {
+                        Console.WriteLine($"ToDo {name} already exists");
+                    }
+                    else
+                    {
+                        todoService.CreateToDo(name, 0, connectionString);
+                        Console.WriteLine("New ToDo successfully created");
+                    }
+                }
+
+                Console.WriteLine("Press enter to continue");
+                Console.Read();
+                break;
+            }
+    }
+}

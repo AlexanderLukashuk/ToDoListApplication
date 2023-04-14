2dba358b581b5d76a140aa55d8e3622bb5be5fd7
AlexanderLukashuk <upiter465@gmail.com>
2023-04-14 14:26:01 +0600

    Created UserService class and CreateUser method

---
+++ MyOwnToDoListApplication/Srvices/UserService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace MyOwnToDoListApplication.Srvices
+{
+	public class UserService
+	{
+		public UserService()
+		{
+		}
+
+		public void CreateUser(string email, string password, string connectionString)
+		{
+			using (var connection = new SqlConnection(connectionString))
+			{
+				try
+				{
+					connection.Open();
+
+					string query =
+                        "INSERT INTO Users (Email, Password)" +
+                        "VALUES (@email, @password);";
+
+					using (var command = new SqlCommand(query, connection))
+					{
+						command.Parameters.AddWithValue("@email", email);
+						command.Parameters.AddWithValue("@password", password);
+
+						command.ExecuteNonQuery();
+					}
+                }
+				catch(SqlException ex)
+				{
+					Console.WriteLine("ERROR: " + ex.Message);
+				}
+				finally
+				{
+					connection.Close();
+				}
+			}
+		}
+	}
+}
+
